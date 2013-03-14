f1c069f04c84488ac9ee18d8e5a992888109d804
Paul Lammertsma <paul@pixplicity.com>
2013-03-14 01:16:49 +0100

    generalized typeface setter to allow for two new components with minimal code: FontEditText and FontButton added layout to illustrate these components

---
+++ src/com/pixplicity/mdevcon2013/widgets/FontButton.java
@@ -0,0 +1,30 @@
+package com.pixplicity.mdevcon2013.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import com.pixplicity.mdevcon2013.util.FontUtil;
+
+public class FontButton extends Button {
+
+	public FontButton(Context context) {
+		super(context);
+		FontTextView.init(this, context, null, 0);
+	}
+
+	public FontButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		FontTextView.init(this, context, attrs, 0);
+	}
+
+	public FontButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		FontTextView.init(this, context, attrs, defStyle);
+	}
+
+	public void setTypeface(String typeface) {
+		FontUtil.setTypeface(this, typeface);
+	}
+
+}
--- src/com/pixplicity/mdevcon2013/widgets/FontTextView.java
+++ src/com/pixplicity/mdevcon2013/widgets/FontTextView.java
@@ -12,20 +12,21 @@ public class FontTextView extends TextView {
 
 	public FontTextView(Context context) {
 		super(context);
-		init(context, null, 0);
+		FontTextView.init(this, context, null, 0);
 	}
 
 	public FontTextView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		init(context, attrs, 0);
+		FontTextView.init(this, context, attrs, 0);
 	}
 
 	public FontTextView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		init(context, attrs, defStyle);
+		FontTextView.init(this, context, attrs, defStyle);
 	}
 
-	private void init(Context context, AttributeSet attrs, int defStyle) {
+	protected static void init(TextView view, Context context,
+			AttributeSet attrs, int defStyle) {
 		if (attrs != null) {
 			final TypedArray a = context.obtainStyledAttributes(
 					attrs, R.styleable.FontTextView, defStyle, 0);
@@ -34,7 +35,7 @@ public class FontTextView extends TextView {
 			a.recycle();
 
 			if (typeface != null) {
-				setTypeface(typeface);
+				FontUtil.setTypeface(view, typeface);
 			}
 		}
 	}
