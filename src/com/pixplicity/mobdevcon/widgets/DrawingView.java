12121ce30e3ff7d67a5ce242b5aa45cddebafa04
Paul Lammertsma <pflammertsma@gmail.com>
2013-07-09 13:24:59 +0200

    Experimenting with quadratic lines and invalidation regions

--- src/com/pixplicity/mobdevcon/widgets/DrawingView.java
+++ src/com/pixplicity/mobdevcon/widgets/DrawingView.java
@@ -11,14 +11,19 @@ import android.view.View;
 
 public class DrawingView extends View {
 
-	private Paint paint = new Paint();
-	private Path path = new Path();
+	private static final boolean QUAD_LINES = false;
+	private static final float STROKE_WIDTH = 6f;
+	private static final boolean INVALIDATE_REGION = true;
+
+	private final Paint paint = new Paint();
+	private final Path path = new Path();
+	private float lastX = -1, lastY;
 
 	public DrawingView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
 		paint.setAntiAlias(true);
-		paint.setStrokeWidth(6f);
+		paint.setStrokeWidth(STROKE_WIDTH);
 		paint.setColor(Color.BLACK);
 		paint.setStyle(Paint.Style.STROKE);
 		paint.setStrokeJoin(Paint.Join.ROUND);
@@ -26,18 +31,46 @@ public class DrawingView extends View {
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		float x = event.getX();
+		float y = event.getY();
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-			path.moveTo(event.getX(), event.getY());
+			path.moveTo(x, y);
 			break;
 		case MotionEvent.ACTION_MOVE:
-			path.lineTo(event.getX(), event.getY());
+			if (QUAD_LINES) {
+				path.quadTo(x, y, (lastX + x) / 2, (lastY + y) / 2);
+			} else {
+				path.lineTo(x, y);
+			}
 			break;
 		case MotionEvent.ACTION_UP:
 			// Nothing to do
 			break;
 		}
-		invalidate();
+		if (INVALIDATE_REGION) {
+			int pad = (int) (STROKE_WIDTH / 2);
+			int l, t, r, b;
+			if (lastX < x) {
+				l = (int) Math.floor(lastX);
+				r = (int) Math.ceil(x);
+			} else {
+				l = (int) Math.floor(x);
+				r = (int) Math.ceil(lastX);
+			}
+			if (lastY < y) {
+				t = (int) Math.floor(lastY);
+				b = (int) Math.ceil(y);
+			} else {
+				t = (int) Math.floor(y);
+				b = (int) Math.ceil(lastY);
+			}
+			invalidate(l - pad, t - pad, r + pad, b + pad);
+		} else {
+			invalidate();
+		}
+		lastX = x;
+		lastY = y;
 		return true;
 	}
 
